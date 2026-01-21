653ade14ce47ccf703f566c4062c7a8f258d5cf4
Nam Phong <nguyennamphong210@gmail.com>
2026-01-21 10:11:38 +0700

    fix oauth routes

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -10,11 +10,14 @@ router.post('/staff-login', staffLogin);
 router.post('/member-register', memberRegister);
 
 router.get('/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
-router.get('/google/success', googleSuccess);
+router.get('/google/callback', passport.authenticate('google', {session: false, failureRedirect: 'http://localhost:5173/login'}),
+googleSuccess);
 
 router.get('/facebook', passport.authenticate('facebook', { scope: ['profile'] }));
-router.get('/facebook/success', facebookSuccess);
+router.get('/facebook/callback', passport.authenticate('facebook', {session: false, failureRedirect: 'http://localhost:5173/login'}),
+facebookSuccess);
 
 router.post('/logout', authMiddleware, logOut);
 
 module.exports = router; 
+
