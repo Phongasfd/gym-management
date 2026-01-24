1f045e0e66d964668f562f04b56bdf61e70e14c7
Nam Phong <nguyennamphong210@gmail.com>
2026-01-24 14:42:57 +0700

    fix eslint config

--- backend/eslint.config.mjs
+++ backend/eslint.config.mjs
@@ -3,6 +3,27 @@ import globals from "globals";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
-  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
+  // Node.js
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: {
+      sourceType: "commonjs",
+      globals: {
+        ...globals.node,      // process, __dirname, etc.
+      },
+    },
+  },
+
+  // Jest
+  {
+    files: ["**/*.test.js", "**/*.spec.js"],
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.jest,     // describe, test, expect
+      },
+    },
+  },
 ]);
