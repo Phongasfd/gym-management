dd368b1a094e88c6cd1161e14c269b793f11bd46
Nam Phong <nguyennamphong210@gmail.com>
2026-01-24 14:50:59 +0700

    fix eslint config

--- backend/eslint.config.cjs
+++ backend/eslint.config.cjs
@@ -1,28 +1,25 @@
-import js from "@eslint/js";
-import globals from "globals";
-import { defineConfig } from "eslint/config";
+const js = require("@eslint/js");
+const globals = require("globals");
+const { defineConfig } = require("eslint/config");
 
-module.exports =  defineConfig([
-  // Node.js
+module.exports = defineConfig([
   {
-    files: ["**/*.{js,mjs,cjs}"],
+    files: ["**/*.{js,cjs}"],
     plugins: { js },
     extends: ["js/recommended"],
     languageOptions: {
       sourceType: "commonjs",
       globals: {
-        ...globals.node,      // process, __dirname, etc.
+        ...globals.node,
       },
     },
   },
-
-  // Jest
   {
     files: ["**/*.test.js", "**/*.spec.js"],
     languageOptions: {
       globals: {
         ...globals.node,
-        ...globals.jest,     // describe, test, expect
+        ...globals.jest,
       },
     },
   },
