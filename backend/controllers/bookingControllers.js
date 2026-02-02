0348950f4d2f176c96f752ad13b282d139169d3a
Nam Phong <nguyennamphong210@gmail.com>
2026-02-02 22:46:04 +0700

    set up routes and controllers for booking

--- backend/controllers/bookingControllers.js
+++ backend/controllers/bookingControllers.js
@@ -1,16 +1,69 @@
 const prisma = require('../prisma');
 
 // Get all bookings
-const getAllBookings = async (req, res) => {};
+const getAllBookings = async (req, res) => {
+  try {
+    const bookings = await prisma.booking.findMany();
+    res.status(200).json(bookings);
+    
+  } catch (error) {
+    res.status(500).json({ msg: 'Server error' });
+  }
+};
 
 // Get booking by ID
-const getBookingById = async (req, res) => {};
+const getBookingById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const booking = await prisma.booking.findUnique({
+      where: { id: id }
+    });
+    if (!booking) {
+      return res.status(404).json({ msg: 'Booking not found' });
+    }
+    res.status(200).json(booking);
+    
+  } catch (error) {
+    res.status(500).json({ msg: 'Server error' });
+  }
+};
 
 // Member booking
-const createBooking = async (req, res) => {};
+const createBooking = async (req, res) => {
+  try {
+    const { class_id, member_id, status } = req.body;
+    const newBooking = await prisma.booking.create({
+      data: {
+        member_id,
+        class_id,
+        status
+      }
+    });
+    res.status(201).json(newBooking);
+  } catch (error) {
+    res.status(500).json({ msg: 'Server error' });
+  }
+};
 
 // Update booking by ID
-const updateBooking = async (req, res) => {};
+const updateBooking = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { class_id, member_id, status } = req.body;
+    const updatedBooking = await prisma.booking.update({
+      where: { id: id },
+      data: { 
+        class_id,
+        member_id,
+        status 
+      }
+    });
+    res.status(200).json(updatedBooking);
+
+  }catch (error) {
+    res.status(500).json({ msg: 'Server error' });
+  }
+};
 
 module.exports = {
   getAllBookings,
--- backend/routes/bookingRoutes.js
+++ backend/routes/bookingRoutes.js
@@ -1,5 +1,21 @@
 const express = require('express');
 const router = express.Router();
-const { getAllBookings, getBookingById, createBooking, updateBooking, deleteBooking } = require('../controllers/classControllers');
+const { getAllBookings, getBookingById, createBooking, updateBooking } = require('../controllers/classControllers');
 const { authMiddleware, staffMiddleware } = require('../middleware/authMiddleware');
 
+// GET    /api/bookings
+router.get('/', authMiddleware, staffMiddleware, getAllBookings);
+
+// GET    /api/bookings/:id
+router.get('/:id', authMiddleware, staffMiddleware, getBookingById);
+
+
+// POST   /api/bookings 
+router.post('/', authMiddleware, createBooking);
+
+
+// PATCH  /api/bookings/:id  
+router.get('/:id', authMiddleware, staffMiddleware, updateBooking);
+
+
+module.exports = router;
