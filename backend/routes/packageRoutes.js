9d4ccadf45731be96a7de8ec039b6eda99cf360a
Nam Phong <nguyennamphong210@gmail.com>
2026-02-02 21:25:28 +0700

    set routes for package

--- backend/routes/packageRoutes.js
+++ backend/routes/packageRoutes.js
@@ -3,3 +3,21 @@ const router = express.Router();
 const {createPackage, getAllPackages, getPackageById, updatePackage, deletePackage} = require('../controllers/packageControllers');
 const { authMiddleware, staffMiddleware } = require('../middleware/authMiddleware'); 
 
+// GET    /api/package
+router.get('/', authMiddleware, getAllPackages); 
+
+// GET    /api/package/:id
+router.get('/:id', authMiddleware, getPackageById);
+
+
+// POST   /api/package
+router.post('/', authMiddleware, staffMiddleware, createPackage);
+
+// patch  /api/package/:id
+router.patch('/:id', authMiddleware, staffMiddleware, updatePackage);
+
+// DELETE /api/package/:id
+router.delete('/:id', authMiddleware, staffMiddleware, deletePackage);
+
+module.exports = router;
+
