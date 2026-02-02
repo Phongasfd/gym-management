121616810c50028b178c262bee209e799519a732
Nam Phong <nguyennamphong210@gmail.com>
2026-02-02 22:26:31 +0700

    set up routes and controllers for checkin

--- backend/controllers/checkinControllers.js
+++ backend/controllers/checkinControllers.js
@@ -1,10 +1,34 @@
 const prisma = require('../prisma');
 
 // Get all checkins
-const getAllCheckins = async (req, res) => {};
+const getAllCheckins = async (req, res) => {
+  try {
+    const checkins = await prisma.checkin.findMany();
+    res.status(200).json(checkins);
+    
+  } catch (error) {
+    res.status(500).json({ msg: 'Server error'});
+  }
+};
 
 // Create new checkins
-const createCheckin = async (req, res) => {};
+const createCheckin = async (req, res) => {
+  try {
+
+    const {member_id, checkin_time, source} = req.body;
+    const newCheckin = await prisma.checkin.create({
+      data: {
+        member_id,
+        checkin_time,
+        source
+      }
+    });
+    res.status(201).json(newCheckin);
+    
+  } catch (error) {
+    res.status(500).json({ msg: 'Server error'});
+  }
+};
 
 module.exports = {
   getAllCheckins,
--- backend/routes/checkinRoutes.js
+++ backend/routes/checkinRoutes.js
@@ -3,3 +3,10 @@ const router = express.Router();
 const { getAllCheckins, createCheckin } = require('../controllers/checkinControllers');
 const { authMiddleware, staffMiddleware } = require('../middleware/authMiddleware');
 
+// GET    /api/checkins   
+router.get('/', authMiddleware, staffMiddleware, getAllCheckins);                
+
+// POST   /api/checkins                  
+router.post('/', authMiddleware, staffMiddleware, createCheckin);
+
+module.exports = router;
