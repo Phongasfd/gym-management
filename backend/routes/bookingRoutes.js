9d8abb7b128d410451287559f276abbd2c426bc2
Nam Phong <nguyennamphong210@gmail.com>
2026-02-03 10:18:51 +0700

    fix path

--- backend/routes/bookingRoutes.js
+++ backend/routes/bookingRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { getAllBookings, getBookingById, createBooking, updateBooking } = require('../controllers/classControllers');
+const { getAllBookings, getBookingById, createBooking, updateBooking } = require('../controllers/bookingControllers');
 const { authMiddleware, staffMiddleware } = require('../middleware/authMiddleware');
 
 // GET    /api/bookings
