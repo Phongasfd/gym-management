a0d2aeaa6d649099909c0ab8e82401e07d1d542c
Nam Phong <nguyennamphong210@gmail.com>
2026-02-13 16:45:37 +0700

    inject font in html tag

--- frontend-user-next/src/app/layout.js
+++ frontend-user-next/src/app/layout.js
@@ -21,14 +21,14 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" className={`${manrope.variable} ${inter.variable}`}>
       <head>
         <link
             rel="stylesheet"
             href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
           />
       </head>
-      <body className={`${manrope.variable} ${inter.variable}`}>
+      <body>
         <header className="header">
           <div className="container">
               <nav className="navbar">
