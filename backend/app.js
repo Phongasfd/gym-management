398edeb67a46a4d34f6b254b877c99271920b52f
Nam Phong <nguyennamphong210@gmail.com>
2026-03-15 09:51:49 +0700

    remove rate limiting and request logging middleware from tests

--- backend/app.js
+++ backend/app.js
@@ -12,8 +12,6 @@ const app = express();
 const cookieParser = require('cookie-parser');
 // Middleware to parse cookies from incoming requests
 
-// Rate limiting (global)
-const { globalLimiter } = require('./middleware/rateLimit');
 
 // Allow request from other port, server
 app.use(cors({
@@ -29,16 +27,24 @@ app.use(express.json());
 app.use(cookieParser());
 // Use cookie parser middleware
 
-// log every request
-const requestLogger = require('./middleware/requestLogger');
-app.use(requestLogger);
 
-// Apply global rate limiter to all routes
-app.use(globalLimiter);
+
+if (process.env.NODE_ENV !== 'test') {
+    // Rate limiting (global)
+    const { globalLimiter } = require('./middleware/rateLimit');
+    // Apply global rate limiter to all routes
+    app.use(globalLimiter);
+}
 
 app.get('/', (req, res) => {
     res.send('Gym Management System Backend is running');
 });
+
+if (process.env.NODE_ENV !== 'test') {
+    // log every request
+    const requestLogger = require('./middleware/requestLogger');
+    app.use(requestLogger);
+}
 
 const auth = require('./routes/authRoutes');
 app.use('/api/auth', auth);
@@ -69,12 +75,14 @@ app.use('/api/classes', classes);
 const booking = require('./routes/bookingRoutes');
 app.use('/api/bookings', booking);
 
-const paymentRoutes = require('./routes/vnpayRoutes');
-app.use('/api/vnpay', paymentRoutes); 
+if(process.env.NODE_ENV !== 'test') {
+    const paymentRoutes = require('./routes/vnpayRoutes');
+    app.use('/api/vnpay', paymentRoutes); 
 
-// AI advisory
-const aiRoutes = require('./routes/aiRoutes');
-app.use('/api/ai', aiRoutes);
+    // AI advisory
+    const aiRoutes = require('./routes/aiRoutes');
+    app.use('/api/ai', aiRoutes);
+}
 
 // global error handler (must be added after all routes)
 const errorHandler = require('./middleware/errorHandler');
--- backend/tests/unit/update_password_staff.unit.test.js
+++ backend/tests/unit/update_password_staff.unit.test.js
@@ -1,5 +1,4 @@
 process.env.JWT_SECRET = "test_secret";
-process.env.OPENAI_API_KEY="test_openai_key";
 
 jest.mock("../../prismaClient", () => ({
   user: {
